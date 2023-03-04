5780de3bcafd8a76c943f16600738d5101dc5875
Heena Syed <heena.afreen909@gmail.com>
2023-03-04 14:21:21 +0530

    final cart appearancess

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,12 @@ import React from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
-const Header = ({userName}) => {
-  const state = useSelector((state)=>state.cart)
+const Header = ({ userName }) => {
+  const state = useSelector((state) => state.cart);
   const location = useLocation();
   console.log(location);
- 
 
   return (
     <div className="header">
@@ -17,19 +16,30 @@ const Header = ({userName}) => {
           <span>SHOP</span>LANE
         </h1>
       </div>
-      
 
-      {!userName? <div className="dropdown">
-        <button className="dropbtn">
-          {" "}
-          <FaRegUserCircle /> <b>Login</b> or<br></br>SignUp
-        </button>
-       <div className="dropdown-content">
-          <a href="\login">LogIn</a>
-          <a href="\signUp">SignUp</a>
-        </div>
-      </div> :<div><p className="email">{userName}</p>  <NavLink to="/login" className="btn btn-outline-dark">Logout</NavLink> </div>}
-      <div className="cart">
+      <div className="dropmenu">
+        {!userName ? (
+          <div className="dropdown">
+            <button className="dropbtn">
+              {" "}
+              <FaRegUserCircle /> <b>Login</b> or<br></br>SignUp
+            </button>
+            <div className="dropdown-content">
+              <NavLink to="/login">LogIn</NavLink>
+              <NavLink to="/SignUp">SignUp</NavLink>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p className="email">{userName}</p>{" "}
+            <NavLink to="/login" className="btn btn-outline-dark">
+              Logout
+            </NavLink>{" "}
+          </div>
+        )}
+      </div>
+
+      <div className="shoppingcart">
         <NavLink to="/cart" className="btn btn-outline-dark ms-2">
           <i className="fa fa-shopping-cart me-1"></i>Cart({state.length})
         </NavLink>
